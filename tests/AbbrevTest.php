e63c3f3a03322bed241e42df4baa9566dc463bec
Kamran Ahmed <kamranahmed.se@gmail.com>
2016-09-21 05:26:45 +0000

    Add tests

---
+++ tests/AbbrevTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace KamranAhmed\Abbrev;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Class AbbrevTest
+ *
+ * @package KamranAhmed\Abbrev
+ */
+class AbbrevTest extends PHPUnit_Framework_TestCase
+{
+
+
+    public function testCanGenerateAbbreviations()
+    {
+        $this->assertTrue(true);
+    }
+}
